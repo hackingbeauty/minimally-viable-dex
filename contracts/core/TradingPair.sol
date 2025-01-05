c63fc3e7014ebe8ab9fd4aa57872fb80336be2ee
Mark Muskardin <markmuskardin@gmail.com>
2025-01-04 19:11:34 -0800

    removed code that will be filled out during lessons

--- contracts/core/TradingPair.sol
+++ contracts/core/TradingPair.sol
@@ -96,84 +96,16 @@ contract TradingPair is ITradingPair, LiquidityTokenERC20 {
     }
 
     function mint(address to) external lock returns (uint liquidity) {
-        (uint112 _reserve0, uint112 _reserve1) = getReserves(); // gas savings
-        uint balance0 = IERC20(token0).balanceOf(address(this));
-        uint balance1 = IERC20(token1).balanceOf(address(this));
-        uint amount0 = balance0.sub(_reserve0);
-        uint amount1 = balance1.sub(_reserve1);
-
-        bool feeOn = _mintFee(_reserve0, _reserve1);
-        uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
-        if (_totalSupply == 0) {
-            liquidity = Math.sqrt(amount0.mul(amount1)).sub(MINIMUM_LIQUIDITY);
-           _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
-        } else {
-            liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
-        }
-        require(liquidity > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED');
-        _mint(to, liquidity);
-
-        _update(balance0, balance1);
-        if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
-        emit Mint(msg.sender, amount0, amount1);
+        // Add code for minting Liquidy Tokens here...
     }
 
     function burn(address to) external lock returns (uint amountASent, uint amountBSent) {
-        (uint112 _reserve0, uint112 _reserve1) = getReserves();
-        address _token0 = token0;
-        address _token1 = token1;
-        uint balance0 = IERC20(_token0).balanceOf(address(this));
-        uint balance1 = IERC20(_token1).balanceOf(address(this));
-        uint liquidity = balanceOf[address(this)];
-
-        bool feeOn = _mintFee(_reserve0, _reserve1);
-        uint _totalSupply = totalSupply;
-        amountASent = (liquidity * balance0) / _totalSupply;
-        amountBSent = (liquidity * balance1) / _totalSupply;
-        require(amountASent > 0 && amountBSent > 0, 'DEX: INSUFFICENT_LIQUIDITY_BURNED');
-
-        _burn(address(this), liquidity);
-        _safeTransfer(_token0, to, amountASent);
-        _safeTransfer(_token1, to, amountBSent);
-
-        balance0 = IERC20(_token0).balanceOf(address(this));
-        balance1 = IERC20(_token1).balanceOf(address(this));
-        _update(balance0, balance1);
-
-        if(feeOn) { kLast = Math.mul(_reserve0, _reserve1); } // _reserve0 and _reserve1 are up-to-date
-        emit Burn(msg.sender, amountASent, amountBSent, to);
+        // Add code for burning Liquidity Tokens here...
     }
 
     // this low-level function should be called from a contract which performs important safety checks
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
-        require(amount0Out > 0 || amount1Out > 0, 'DEX: INSUFFICIENT_OUTPUT_AMOUNT');
-        (uint112 _reserve0, uint112 _reserve1) = getReserves(); // gas savings
-        require(amount0Out < _reserve0 && amount1Out < _reserve1, 'DEX: INSUFFICIENT_LIQUIDITY');
-
-        uint balance0;
-        uint balance1;
-        { // scope for _token{0,1}, avoids stack too deep errors
-        address _token0 = token0;
-        address _token1 = token1;
-        require(to != _token0 && to != _token1, 'DEX: INVALID_TO');
-        if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
-        if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-        if (data.length > 0) IFlashSwapCallee(to).flashSwapCall(msg.sender, amount0Out, amount1Out, data);
-        balance0 = IERC20(_token0).balanceOf(address(this));
-        balance1 = IERC20(_token1).balanceOf(address(this));
-        }
-        uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
-        uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
-       
-        require(amount0In > 0 || amount1In > 0, 'DEX: INSUFFICIENT_INPUT_AMOUNT');
-        { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
-        uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
-        uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
-        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'DEX: K');
-        }
-
-        _update(balance0, balance1);
-        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
+        // Add code for swapping/exchange here...
     }
 
     function approve(
--- contracts/periphery/Router.sol
+++ contracts/periphery/Router.sol
@@ -9,8 +9,8 @@ import './libraries/TransferHelper.sol';
 import './interfaces/IWETH.sol';
 
 contract Router is IRouter {
-    address public immutable override factoryAddr;
-    address public immutable override WETH;
+    address public immutable factoryAddr;
+    address public immutable WETH;
 
     constructor(address _factoryAddr, address _WETH) {
         factoryAddr = _factoryAddr;
@@ -34,24 +34,7 @@ contract Router is IRouter {
         uint amountAMin,
         uint amountBMin
     ) internal returns (uint amountA, uint amountB){
-        if(IFactory(factoryAddr).getTradingPair(tokenA, tokenB) == address(0)){
-            IFactory(factoryAddr).createTradingPair(tokenA, tokenB);
-        }
-        (uint reserveA, uint reserveB) = DEXLibrary.getReserves(factoryAddr, tokenA, tokenB);
-        if(reserveA == 0 && reserveB == 0) {
-            (amountA, amountB) = (amountADesired, amountBDesired);
-        } else {
-            uint amountBOptimal = DEXLibrary.quote(amountADesired, reserveA, reserveB);
-            if(amountBOptimal <= amountBDesired) { //tokenB is worth MORE than LP thinks, so a smaller amount is required
-                require(amountBOptimal >= amountBMin, 'DEXLibrary: INSUFFICIENT_B_AMOUNT');
-                (amountA, amountB) = (amountADesired, amountBOptimal);
-            } else { //tokenB is worth LESS than LP thinks, so a higher amount is required
-                uint amountAOptimal = DEXLibrary.quote(amountBDesired, reserveB, reserveA);
-                assert(amountAOptimal <= amountADesired);
-                require(amountAOptimal >= amountAMin, 'DEXLibrary: INSUFFICIENT_A_AMOUNT');
-                (amountA, amountB) = (amountAOptimal, amountBDesired);
-            }
-        }
+        // Add resusable code for determining optimal liquidity deposit amounts here...
     }
 
     function depositLiquidity(
@@ -64,18 +47,7 @@ contract Router is IRouter {
         address to,
         uint deadline
     ) external ensure(deadline) returns(uint amountA, uint amountB, uint liquidity){
-        (amountA, amountB) = _depositLiquidity(
-            tokenA,
-            tokenB,
-            amountADesired,
-            amountBDesired,
-            amountAMin,
-            amountBMin
-        );
-        address pair = DEXLibrary.pairFor(factoryAddr, tokenA, tokenB);
-        TransferHelper.safeTransferFrom(tokenA, msg.sender, pair, amountA);
-        TransferHelper.safeTransferFrom(tokenB, msg.sender, pair, amountB);
-        liquidity = ITradingPair(pair).mint(to);
+        // Add code for depositing liquidity here..
     }
 
     function withdrawLiquidity(
@@ -87,31 +59,12 @@ contract Router is IRouter {
         address to,
         uint deadline
     ) external ensure(deadline) returns (uint amountA, uint amountB) {
-        address pair = DEXLibrary.pairFor(factoryAddr, tokenA, tokenB); 
-        ITradingPair(pair).transferFrom(msg.sender, pair, liquidity); // send liquidity to pair
-        (uint amountASent, uint amountBSent) = ITradingPair(pair).burn(to);
-        (address token0,) = DEXLibrary.sortTokens(tokenA, tokenB);
-        (amountA, amountB) = tokenA == token0 ? (amountASent, amountBSent) : (amountBSent, amountASent);
-        require(amountA >= amountAMin, 'DEX: INSUFFICIENT_A_AMOUNT');
-        require(amountB >= amountBMin, 'DEX: INSUFFICIENT_B_AMOUNT');
+        // Add code for withdrawing liquidity here..
     }
 
     // ************ EXCHANGE ************
     function _swap(uint[] memory amounts, address[] memory path, address _to) internal virtual {
-        for (uint i; i < path.length - 1; i++) {
-            (address input, address output) = (path[i], path[i + 1]);
-            (address token0,) = DEXLibrary.sortTokens(input, output);
-            uint amountOut = amounts[i + 1];
-            (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
-
-            address to = i < path.length - 2 ? DEXLibrary.pairFor(factoryAddr, output, path[i + 2]) : _to;
-            ITradingPair(DEXLibrary.pairFor(factoryAddr, input, output)).swap(
-                amount0Out,
-                amount1Out,
-                to,
-                new bytes(0)
-            );
-        }
+        // Add code for Routing here...
     }
 
     // Allows a trader to specify the exact amount of token "As" a trader is willing to input,
@@ -123,12 +76,7 @@ contract Router is IRouter {
         address to,
         uint deadline
     ) external ensure(deadline) returns (uint[] memory amounts) {         //the various amounts that will be swapped out        
-        amounts = DEXLibrary.getAmountsOut(factoryAddr, amountIn, path);  //from each exchange along the path
-        require(amounts[amounts.length - 1] >= amountOutMin, 'DEX ROUTER: INSUFFICIENT_OUTPUT_AMOUNT');
-        TransferHelper.safeTransferFrom(
-            path[0], msg.sender, DEXLibrary.pairFor(factoryAddr, path[0], path[1]), amounts[0]
-        );
-        _swap(amounts, path, to);
+        // Add code for <enter> here...
     }
 
     function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) 
@@ -137,12 +85,7 @@ contract Router is IRouter {
         ensure(deadline) 
         returns (uint[] memory amounts)
     {
-        require(path[0] == WETH, 'DEX: INVALID_PATH');
-        amounts = DEXLibrary.getAmountsOut(factoryAddr, msg.value, path);   
-        require(amounts[amounts.length - 1] >= amountOutMin, 'DEX: INSUFFICIENT_OUTPUT_AMOUNT');
-        IWETH(WETH).deposit{value: amounts[0]}();
-        assert(IWETH(WETH).transfer(DEXLibrary.pairFor(factoryAddr, path[0], path[1]), amounts[0]));
-        _swap(amounts, path, to);
+        // Add code for <enter> here...
     }
 
 }
